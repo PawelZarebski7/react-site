3f78c1882261276f062f8a72fb5821a861f24ee7
Paweł <pawelzarebski7@wp.pl>
2024-06-08 20:51:26 +0200

    Conntact site

--- src/components/contact/ContactSection.tsx
+++ src/components/contact/ContactSection.tsx
@@ -9,7 +9,7 @@ const ContactSection: React.FC = () => {
             <div className="col-xl-6 col-lg-6 col-12">
               <div className="contact-page google-map">
                 <iframe
-                  src="https://www.google.pl/maps/place/Ogrodowa+7,+86-008+Koronowo/@53.3139821,17.9395485,16z/data=!4m6!3m5!1s0x47030441aa3d36a1:0x187b07a76217a6!8m2!3d53.3147833!4d17.9398275!16s%2Fg%2F11hmqzxf6k?entry=ttu"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2383.5901218107088!2d17.937252576985934!3d53.31478647708413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47030441aa3d36a1%3A0x187b07a76217a6!2sOgrodowa%207%2C%2086-008%20Koronowo!5e0!3m2!1spl!2spl!4v1717869245899!5m2!1spl!2spl"
                   width="600"
                   height="600"
                   style={{ border: 0 }}
@@ -22,24 +22,24 @@ const ContactSection: React.FC = () => {
             <div className="col-xl-6 col-lg-6 col-12">
               <div className="contact-form dark-bg">
                 <div className="section-title">
-                  <h2 className="text-white">Lets talk...</h2>
+                  <h2 className="text-white">Porozmawiajmy</h2>
                 </div>
                 <form>
                   <div className="row">
                     <div className="col-md-6">
-                      <label>Your Name*</label>
-                      <input type="text" placeholder="Your Full Name" />
+                      <label>Twoje imię*</label>
+                      <input type="text" placeholder="Twoje imię" />
                     </div>
                     <div className="col-md-6">
-                      <label>Email Address*</label>
-                      <input type="email" placeholder="E-mail Address" />
+                      <label>Email*</label>
+                      <input type="email" placeholder="E-mail" />
                     </div>
                     <div className="col-12">
-                      <label>Subject*</label>
-                      <input type="text" placeholder="Website" />
+                      <label>Temat*</label>
+                      <input type="text" placeholder="Temat" />
                     </div>
                     <div className="col-12">
-                      <label>Message*</label>
+                      <label>Wiadomość*</label>
                       <textarea
                         name="message"
                         id="message"
@@ -49,8 +49,8 @@ const ContactSection: React.FC = () => {
                       ></textarea>
                     </div>
                     <div>
-                      <button type="submit" className="bordered-btn">
-                        Send Now
+                    <button type="submit" className="bordered-btn" onClick={() => window.location.href = "mailto:pawelzarebski7@wp.pl"}>
+                      Wyślij teraz
                       </button>
                     </div>
                   </div>
